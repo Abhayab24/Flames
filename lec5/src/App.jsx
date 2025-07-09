47c45e27d6bfa331cd7988c1202fb44653234742
Kumar Abhay Partap Singh <abhaypartap542005@gmail.com>
2025-07-09 20:28:07 +0530

    react

--- lec5/src/App.jsx
+++ lec5/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import React from 'react'
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div>
-      
+      <h1 className='text-6xl text-red-700 underline'>Hello World</h1>
     </div>
   )
 }
---
+++ lec5/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfileCard from "../components/ProfileCard"; // ❌ Removed extra space after the quote
+
+function Home(){
+  return (
+    <div>
+      <ProfileCard /> 
+    </div>
+  );
+}
+
+export default Home;
